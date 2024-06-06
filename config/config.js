5eddbcc0c8a58ce4b55b0e28532a698364de4af0
Memoryman2022 <rlacannon@gmail.com>
2024-06-06 11:04:49 +0200

    config

--- config/config.js
+++ config/config.js
@@ -1,11 +1,14 @@
 // config.js
 require("dotenv").config();
 
-const isDevelopment = process.env.NODE_ENV === "development";
-const MONGO_URI = isDevelopment
-  ? process.env.MONGODB_URI_LOCAL
-  : process.env.MONGODB_URI_REMOTE;
+// const isDevelopment = process.env.NODE_ENV === "development"
+const MONGO_URI =
+  process.env.NODE_ENV === "development"
+    ? process.env.MONGODB_URI_LOCAL
+    : process.env.MONGODB_URI_REMOTE;
 
 module.exports = {
   MONGO_URI,
 };
+
+//production
